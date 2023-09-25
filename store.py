2309b3148ca615fdc22a3d00a38b629ab42f5998
vovavolkov <dev@vovavolkov.com>
2023-09-25 14:55:18 +0100

    added 5-sec time intervals to populate tests.db table

--- store.py
+++ store.py
@@ -1,6 +1,7 @@
 import sqlite3
 import argparse
 import random
+import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('table', default="readings", nargs ='?', help='table for readings')
@@ -10,6 +11,8 @@ print(args)
 
 conn = sqlite3.connect('tests.db')
 cursor = conn.cursor()
+
+date = datetime.datetime.now()
 
 cursor.execute(f"CREATE TABLE IF NOT EXISTS {args.table} (id INTEGER PRIMARY KEY AUTOINCREMENT, co2 INTEGER, temperature REAL, humidity REAL, time DATETIME DEFAULT CURRENT_TIMESTAMP)")
 
@@ -21,8 +24,10 @@ for i in range(n):
     co2 = int(random.gauss(1000,100))
     humidity = round(random.gauss(50, 13),2)
     temperature = round(random.gauss(15, 10),2)
-    
-    cursor.execute(f"INSERT INTO {args.table} (co2, temperature, humidity) VALUES ({co2}, {temperature}, {humidity})")
+    date = date +  datetime.timedelta(seconds=5)
+    date_str = date.strftime("%Y-%m-%d %H:%M:%S")
+
+    cursor.execute(f"INSERT INTO {args.table} (co2, temperature, humidity, time) VALUES ({co2}, {temperature}, {humidity}, '{date_str}')")
 
 conn.commit()
 conn.close()
