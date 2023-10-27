b2a17b9c7a2ab2e2496e2f8a41a658542775d92c
vovavolkov <dev@vovavolkov.com>
2023-10-27 10:48:55 +0100

    refactored long strings and added comments

--- sensor.py
+++ sensor.py
@@ -13,19 +13,27 @@ from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1305
 import adafruit_scd4x
 
+# Define available command line arguments and fetch them)
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--dark', action='store_true', help="no display")
-parser.add_argument('table', default="readings", nargs ='?', help='table for readings')
+parser.add_argument(
+    '-d', '--dark',
+    action='store_true', help="no display")
+parser.add_argument(
+    "table", default="readings",
+    nargs="?", help="table for readings")
 args = parser.parse_args()
 
 print(args)
 
+# Connect to the database 'readings.db'
 conn = sqlite3.connect('readings.db')
 cursor = conn.cursor()
-cursor.execute(f"CREATE TABLE IF NOT EXISTS {args.table}"
-               "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
-               "co2 INTEGER, temperature REAL, humidity REAL,"
-               "time DATETIME DEFAULT CURRENT_TIMESTAMP)")
+# Create a table specified in command arguments, if it doesn't exist
+cursor.execute(
+    f"CREATE TABLE IF NOT EXISTS {args.table}"
+    "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    "co2 INTEGER, temperature REAL, humidity REAL,"
+    "time DATETIME DEFAULT CURRENT_TIMESTAMP)")
 
 # try to initialise the sensor, catching errors
 #try:
@@ -42,7 +50,7 @@ print("Sensor connected\nSerial number:", [hex(i) for i in scd4x.serial_number])
 # Define the Reset Pin for the display
 oled_reset = digitalio.DigitalInOut(D4)
 
-# Create the I2C interface for the OLED display.
+# Create the I2C interface for the OLED display
 disp_i2c = busio.I2C(SCL, SDA)
 
 # Create the SSD1305 OLED class.
@@ -52,58 +60,59 @@ disp = adafruit_ssd1305.SSD1305_I2C(128, 32, disp_i2c, reset=oled_reset)
 disp.fill(0)
 disp.show()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
+# Create blank image for drawing
+# Create image with mode '1' for 1-bit color
 width = disp.width
 height = disp.height
 image = Image.new("1", (width, height))
 
-# Get drawing object to draw on image.
+# Get drawing object to draw on image
 draw = ImageDraw.Draw(image)
 
-# Draw a black filled box to clear the canvas for draw method.
+# Draw a black filled box to clear the canvas for draw method
 draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
 # Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
+# First define some constants to allow easy resizing of shapes
 padding = -2
 top = padding
 bottom = height - padding
-# Move left to right keeping track of the current x position for drawing shapes.
+# Move left to right keeping track of the current x position for drawing shapes
 x = 0
 
-# Load default font.
-# font = ImageFont.load_default()
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the
-# same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# Load a .ttf font
 font = ImageFont.truetype('assets/Minecraftia-Regular.ttf', 8)
 
+# Define a function to print out a string with a pixel offset from top
 def drawString(offset, text):
     draw.text((0, top+offset), text, font=font, fill=255)
 
+# Cleanup function - fills display with black, i.e. switches it off
 def cleanup(signal, frame):
-    # clear switch off display before exiting.
     conn.close()
     disp.fill(0)
     disp.show()
     print("\nDisplay turned off, changes commited.")
     exit(0)
 
-# Register the cleanup function to run when the script is terminated.
+# Register the cleanup function to run when the script is terminated
 signal.signal(signal.SIGINT, cleanup)
 signal.signal(signal.SIGTERM, cleanup)
 
+# Function swtiches value of args.dark, i.e. switches the display on/off
 def toggleDisplay(signal, frame):
     args.dark = not args.dark
     print("Display is now:", "OFF" if args.dark else "ON")
     disp.fill(0)
     disp.show()
 
+# Display toggle is triggered by kill -USR1 signal.
 signal.signal(signal.SIGUSR1, toggleDisplay)
 
-sensor_status = (lambda x: "Sensor OK" if x else "NO SENSOR, values random")(sensorOK)
+sensor_status = (
+    lambda x: "Sensor OK" if x
+    else "NO SENSOR, values random")
+    (sensorOK)
 
 #while True:
 #    # clear the canvas
@@ -129,32 +138,33 @@ scd4x.start_periodic_measurement()
 while True:
     if scd4x.data_ready:
 
-        # fetch the values from the sensor
+        # Fetch the readings from the sensor
         co2 = scd4x.CO2
         temperature = round(scd4x.temperature, 4)
         humidity = round(scd4x.relative_humidity, 2)
 
-        # write the values to the database
-        cursor.execute(f"INSERT INTO {args.table}"
-                       f"(co2, temperature, humidity)"
-                       f"VALUES ({co2}, {temperature}, {humidity})")
+        # Inser the new values to the table
+        cursor.execute(
+            f"INSERT INTO {args.table}"
+            "(co2, temperature, humidity, time)"
+            "VALUES ({co2}, {temperature},"
+            "{humidity},'{date_str}')")
         conn.commit() 
 
-        # skip the display if the -d(ark) flag is set
+        # Skip the display if the -d(ark) flag is set
         if args.dark:
             continue
 
-        # print the values on the display 
+        # Create a blank canvas to print out the new values
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
+        # Add text strings to the canvas
         drawString(0, sensor_status)
-
         drawString(8, f"CO2: {co2} ppm")
-
         drawString(16, f"Temp: {temperature} *C")
-
         drawString(25, f"Hum: {humidity} %")
 
+        # Push the canvas onto the display
         disp.image(image)
         disp.show()
 
--- store.py
+++ store.py
@@ -4,30 +4,36 @@ import random
 import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument('table', default="readings", nargs ='?', help='table for readings')
+parser.add_argument(
+    "table", default="readings",
+    nargs="?", help="table for readings")
 args = parser.parse_args()
-
 print(args)
 
-conn = sqlite3.connect('tests.db')
+conn = sqlite3.connect("tests.db")
 cursor = conn.cursor()
-
-date = datetime.datetime.now()
-
-cursor.execute(f"CREATE TABLE IF NOT EXISTS {args.table} (id INTEGER PRIMARY KEY AUTOINCREMENT, co2 INTEGER, temperature REAL, humidity REAL, time DATETIME DEFAULT CURRENT_TIMESTAMP)")
+cursor.execute(
+    f"CREATE TABLE IF NOT EXISTS {args.table}"
+    "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    "co2 INTEGER, temperature REAL, humidity REAL,"
+    "time DATETIME DEFAULT CURRENT_TIMESTAMP)")
 
 print("how many entries?")
-
 n = int(input())
 
+date = datetime.datetime.now()
 for i in range(n):
-    co2 = int(random.gauss(1000,100))
-    humidity = round(random.gauss(50, 13),2)
-    temperature = round(random.gauss(15, 10),2)
-    date = date +  datetime.timedelta(seconds=5)
-    date_str = date.strftime("%Y-%m-%d %H:%M:%S")
+    co2 = int(random.gauss(1000, 100))
+    humidity = round(random.gauss(50, 13), 2)
+    temperature = round(random.gauss(15, 10), 2)
 
-    cursor.execute(f"INSERT INTO {args.table} (co2, temperature, humidity, time) VALUES ({co2}, {temperature}, {humidity}, '{date_str}')")
+    date = date + datetime.timedelta(seconds=5)
+    date_str = date.strftime("%Y-%m-%d %H:%M:%S")
+    cursor.execute(
+        f"INSERT INTO {args.table}"
+        "(co2, temperature, humidity, time)"
+        "VALUES ({co2}, {temperature},"
+        "{humidity},'{date_str}')")
 
 conn.commit()
 conn.close()
