6024ccdf4bd4fa01bf2d51373436c252f3ce2e3b
vovavolkov <dev@vovavolkov.com>
2023-11-15 19:09:33 +0000

    formatting, renamed html template, added date picker

--- flask/hello.py
+++ flask/hello.py
@@ -24,7 +24,7 @@ first_date = times[0].strftime("%Y-%m-%d")
 last_date = times[-1].strftime("%Y-%m-%d")
 
 
-def plot(x_axis,y_axis,x_label,y_label,title):
+def plot(x_axis, y_axis, x_label, y_label, title):
     fig = Figure()
 
     ax = fig.subplots()
@@ -36,13 +36,15 @@ def plot(x_axis,y_axis,x_label,y_label,title):
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return f"data:image/png;base64,{data}"
 
-@app.route('/')
 
-def hello():
-    print("bob")
+@app.route('/')
+def index():
     current_co2 = co2[-1]
-    co2_str= plot(times, co2, "date", "co2", "CO2 over time")
-    temp_str=plot(times, temperature, "date", "temperature", "Temperature over time")
-    hum_str=plot(times, humidity, "date", "humidity", "Humidity over time")
+    co2_str = plot(times, co2, "date", "co2", "CO2 over time")
+    temp_str = plot(times, temperature, "date", "temperature", "Temperature over time")
+    hum_str = plot(times, humidity, "date", "humidity", "Humidity over time")
     images_list = [co2_str, temp_str, hum_str]
-    return render_template('hello.html', co2=current_co2, images=images_list)
+    return render_template(
+        'index.html', co2=current_co2, first_date=first_date,
+        last_date=last_date, images=images_list
+    )
