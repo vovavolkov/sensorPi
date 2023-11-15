796e06861c14d4f353643acd309a7d9eb864c587
vovavolkov <dev@vovavolkov.com>
2023-11-15 20:51:19 +0000

    reformat

--- sensor.py
+++ sensor.py
@@ -22,7 +22,6 @@ parser.add_argument(
     "table", default="readings",
     nargs="?", help="table for readings")
 args = parser.parse_args()
-
 print(args)
 
 # Connect to the database 'readings.db'
@@ -35,25 +34,13 @@ cursor.execute(
     "co2 INTEGER, temperature REAL, humidity REAL,"
     "time DATETIME DEFAULT CURRENT_TIMESTAMP)")
 
-#try to initialise the sensor, catching errors
-try:
-    sensor_i2c = board.I2C()
-    scd4x  = adafruit_scd4x.SCD4X(sensor_i2c)
-    sensorOK = True
-    print("Sensor connected\nSerial number:", [hex(i) for i in scd4x.serial_number])
-
-except Exception:
-    sensorOK=False
-    print("Sensor not initialised")
-
-
 # Define the Reset Pin for the display
 oled_reset = digitalio.DigitalInOut(D4)
 
-# Create the I2C interface for the OLED display
+# Create the I2C interface for the display
 disp_i2c = busio.I2C(SCL, SDA)
 
-# Create the SSD1305 OLED class.
+# Create the SSD1305 display class.
 disp = adafruit_ssd1305.SSD1305_I2C(128, 32, disp_i2c, reset=oled_reset) 
 
 # Clear display
@@ -83,45 +70,54 @@ x = 0
 # Load a .ttf font
 font = ImageFont.truetype('assets/Minecraftia-Regular.ttf', 8)
 
+
 # Define a function to print out a string with a pixel offset from top
-def drawString(offset, text):
+def draw_string(offset, text):
     draw.text((0, top+offset), text, font=font, fill=255)
+
+
+# try to initialise the sensor, catching errors
+try:
+    sensor_i2c = board.I2C()
+    scd4x = adafruit_scd4x.SCD4X(sensor_i2c)
+    sensorOK = True
+    print("Sensor connected\nSerial number:", [hex(i) for i in scd4x.serial_number])
+
+except Exception:
+    sensorOK = False
+    print("Sensor not found")
+    if not args.dark:
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        draw_string(0, "Sensor not found")
+        disp.image(image)
+        disp.show()
+    exit(1)
+
 
 # Cleanup function - fills display with black, i.e. switches it off
 def cleanup(signal, frame):
     conn.close()
     disp.fill(0)
     disp.show()
-    print("\nDisplay turned off, changes commited.")
+    print("\nDisplay turned off, changes committed.")
     exit(0)
+
 
 # Register the cleanup function to run when the script is terminated
 signal.signal(signal.SIGINT, cleanup)
 signal.signal(signal.SIGTERM, cleanup)
 
-# Function swtiches value of args.dark, i.e. switches the display on/off
-def toggleDisplay(signal, frame):
+
+# Function switches value of args.dark, i.e. switches the display on/off
+def toggle_display(signal, frame):
     args.dark = not args.dark
     print("Display is now:", "OFF" if args.dark else "ON")
     disp.fill(0)
     disp.show()
 
+
 # Display toggle is triggered by kill -USR1 signal.
-signal.signal(signal.SIGUSR1, toggleDisplay)
-
-sensor_status = (
-    lambda x: "Sensor OK" if x
-    else "NO Sensor"(sensorOK))
-
-
-if not sensorOK:
-    print(sensor_status)
-    if not args.dark:
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        drawString(0, sensor_status)
-        disp.image(image)
-        disp.show()
-    exit(1)
+signal.signal(signal.SIGUSR1, toggle_display)
 
 scd4x.start_periodic_measurement()
 
@@ -135,7 +131,7 @@ while True:
         # Insert the new values into the table
         cursor.execute(
             f"INSERT INTO {args.table}"
-            f"(co2, temperature, humidity)"
+            "(co2, temperature, humidity)"
             f"VALUES ({co2}, {temperature}, {humidity})")
         conn.commit() 
 
@@ -147,10 +143,10 @@ while True:
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
         # Add text strings to the canvas
-        drawString(0, sensor_status)
-        drawString(8, f"CO2: {co2} ppm")
-        drawString(16, f"Temp: {temperature} *C")
-        drawString(25, f"Hum: {humidity} %")
+        draw_string(0, "Sensor OK")
+        draw_string(8, f"CO2: {co2} ppm")
+        draw_string(16, f"Temp: {temperature} *C")
+        draw_string(25, f"Hum: {humidity} %")
 
         # Push the canvas onto the display
         disp.image(image)
