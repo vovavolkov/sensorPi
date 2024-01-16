26948048a863a31990a7b5828b4811e6865fb6d8
vovavolkov <dev@vovavolkov.com>
2024-01-16 09:38:29 +0000

    remove example code

--- sensorPi/__init__.py
+++ sensorPi/__init__.py
@@ -35,9 +35,4 @@ def create_app(test_config=None):
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     return app
