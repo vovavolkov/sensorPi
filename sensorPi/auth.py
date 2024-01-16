86dc8eb74ff4da35f12ef03dac4d6b9f5ad4e8f4
vovavolkov <dev@vovavolkov.com>
2024-01-16 09:39:49 +0000

    replace registration with create_user

--- sensorPi/auth.py
+++ sensorPi/auth.py
@@ -34,34 +34,25 @@ def login():
     return render_template('auth/login.html')
 
 
-@bp.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
+def create_user(username, password):
+    db = get_db()
+    error = None
 
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
+    if not username:
+        error = 'Username is required.'
+    elif not password:
+        error = 'Password is required.'
 
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password, method='pbkdf2:sha3_512', salt_length=8))
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for("auth.login"))
-
-        flash(error)
-
-    return render_template('auth/register.html')
+    if error is None:
+        try:
+            db.execute(
+                "INSERT INTO user (username, password) VALUES (?, ?)",
+                (username, generate_password_hash(password, method='pbkdf2:sha3_512', salt_length=8))
+            )
+            db.commit()
+        except db.IntegrityError:
+            error = f"User {username} is already registered."
+    return error
 
 
 @bp.before_app_request
