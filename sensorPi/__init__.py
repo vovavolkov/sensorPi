6845adda220c3bb592e371fa2952e660ba69f5db
vovavolkov <dev@vovavolkov.com>
2024-02-05 14:54:28 +0000

    refactoring

--- sensorPi/__init__.py
+++ sensorPi/__init__.py
@@ -8,6 +8,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
+        # replace for production
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'readings.db'),
     )
@@ -26,26 +27,23 @@ def create_app(test_config=None):
         pass
 
     from . import db
-
-    db.init_app(app)
-
     from . import auth
     from . import blog
     from . import hardware
 
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     hardware.init_app(app)
-    
+
     # threading – background function
     def background():
         # wrap the hardware module with app context, allowing it to use the db
         with app.app_context():
             my_database = db.get_db()
             hardware.start_measuring(my_database)
-
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
-
     # start a background thread with hardware interaction module
     b = threading.Thread(name='background', target=background)
     b.start()
